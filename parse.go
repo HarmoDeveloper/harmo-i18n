3bd8926ca1c2370b89b849c84c379511292bba05
rhuan-harmo <rhuan.ferrer@harmo.me>
2024-05-27 18:16:11 -0300

    created internationalization go package

---
+++ parse.go
@@ -0,0 +1,40 @@
+package i18n
+
+import "github.com/HarmoDeveloper/harmo-i18n/langs"
+
+var ParserGlobal *Parser
+
+type Parser struct {
+	countries         map[string]string
+	invertedCountries map[string]string
+}
+
+func (p Parser) Parse(id string) string {
+	if name, ok := p.countries[id]; ok {
+		return name
+	}
+
+	if code, ok := p.invertedCountries[id]; ok {
+		return code
+	}
+	return ""
+}
+
+func NewParser() *Parser {
+
+	invertedCountries := make(map[string]string)
+
+	for code, name := range langs.Countries {
+		invertedCountries[name] = code
+	}
+
+	return &Parser{
+		countries:         langs.Countries,
+		invertedCountries: invertedCountries,
+	}
+}
+
+func Parse(id string) string {
+	parser := NewParser()
+	return parser.Parse(id)
+}
